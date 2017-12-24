c6e827445cb8a01ca35d84c1c55f682c37466ec4
btaba <btabanpour@gmail.com>
2017-12-23 21:08:50 -0500

    bump tag 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 def setup_package():
     config = {
         'name': 'yarlp',
-        'version': '0.0.2',
+        'version': '0.0.3',
         'description': 'yarlp',
         'author': 'Baruch Tabanpour',
         'author_email': '',
